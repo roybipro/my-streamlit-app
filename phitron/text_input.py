a70d807353c7afd279bb7544d10778139451958a
Roy Bipro <roybipro896@gmail.com>
2026-04-18 20:48:09 +0600

    lean to use streamlit

---
+++ phitron/text_input.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("My First Streamlit App",anchor=False)
+st.header("Content 1",divider=True)
+st.subheader("Subheader 1")
+st.text("This is a text")
+
+st.markdown( "**Hello** ***world*** :red[My] :red-background[Boy] How are you? :alien:" )
